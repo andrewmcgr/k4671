b1485546852f25e203d8ac1b39b39c3ebc38365e
Andrew McGregor <andrewmcgr@gmail.com>
2025-09-28 20:25:05 +1000

    Updates for installation via DFU and trsync stubs

--- src/stepper_commands.rs
+++ src/stepper_commands.rs
@@ -151,3 +151,34 @@ klipper_enumeration! {
         Endstop,
     }
 }
+
+#[klipper_command]
+pub fn config_trsync(_context: &mut State, _oid: u8) {
+    // TODO:
+}
+
+#[klipper_command]
+pub fn trsync_start(
+    _context: &mut State,
+    _oid: u8,
+    _report_clock: u32,
+    _report_ticks: u32,
+    _expire_reason: u8,
+) {
+    // TODO:
+}
+
+#[klipper_command]
+pub fn trsync_set_timeout(_context: &mut State, _oid: u8, _clock: u32) {
+    // TODO:
+}
+
+#[klipper_command]
+pub fn trsync_trigger(_context: &mut State, _oid: u8, _reason: u8) {
+    // TODO:
+}
+
+#[klipper_command]
+pub fn trsync_state(_context: &mut State, _oid: u8, _can_trigger: u8, _trigger_reason: u8, _clock: u32) {
+    // TODO:
+}
